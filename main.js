7482fbf6cc9f9c774b2bafc449bf917bccb3add5
Davidchhing <davidhchhing@gmail.com>
2018-02-11 12:51:56 -0800

    completed assignment 4

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-var username = prompt("What is your name?:")
-alert("welcome to my site " + username);
-console.log("Visitors name:" + username);
-var wantToPlay = confirm("welcome to my site " + username + "\n\n Do you want to play my guessing game?")
+var userName = prompt("What is your name?:")
+alert("welcome to my site " + userName);
+document.getElementById("userName").innerHTML = userName;
+console.log("Visitors name:" + userName);
+var wantToPlay = confirm("welcome to my site " + userName + "\n\n Do you want to play my guessing game?")
 console.log("Wanted to play?" + wantToPlay);
 if (wantToPlay) {
     alert("Let's get started then!");
     var counter = 0;
-    
+
     var favoriteFood = confirm("My favorite food is sushi??");
     console.log("Favorite food is sushi" + favoriteFood);
     var answer;
-    
+
     if (favoriteFood) {
-        answer = "Yes I love Sushi" ;   
+        answer = "Yes I love Sushi";
         counter++;
     } else {
         answer = "Nope that's wrong, I love sushi";
     }
-
-    document.getElementById("sushi").innerHTML = answer; 
+    document.getElementById("sushi").innerHTML = answer;
 
     var bornOregon = confirm("Was I born and raised in Oregon?");
     console.log("Born in oregon" + bornOregon)
@@ -29,7 +29,6 @@ if (wantToPlay) {
     } else {
         answer = "Wrong, I was born in Oregon";
     }
-    
     document.getElementById("born").innerHTML = answer;
 
     var likeDogs = confirm("Do I like dogs?");
@@ -54,15 +53,15 @@ if (wantToPlay) {
     } else if (age > myAge) {
         answer = "Nope " + myAge + "is too low";
     } else if (age < myAge) {
-        answer =  "Close, but your guess " + myAge + " is too high"
+        answer = "Close, but your guess " + myAge + " is too high"
     } else {
-          "I'm not sure what you typed in.";
+        "I'm not sure what you typed in.";
     }
     document.getElementById("my-age").innerHTML = answer;
 
     var pho = confirm("Does my family own a chain of PHO restaurants?");
     console.log("Does my family own a chain of PHO restaurants?" + pho);
-    
+
     if (pho) {
         answer = "No my family doesn't own any Pho restaurants, but that would be awesome if they did";
     } else {
@@ -71,19 +70,31 @@ if (wantToPlay) {
     }
     document.getElementById("pho-restaurant").innerHTML = answer;
 
-    var movies = prompt("What is my favorite movie genre? \n\n Horror, Action, Comedy, Romance, Sci-fi, Indies");
-    console.log("What is my favorite movie genre?" + movies)
-    var genre = ['Action', 'Comedy', 'Romance', 'Sci-fi', 'Indies'];
-    var myMovieGenre = "horror";
+    var movieGuess = prompt("What is my favorite movie genre? \n\n Horror, Action, Comedy, Romance, Sci-fi, Indies");
+    console.log("What is my favorite movie genre?" + movieGuess)
+    var genre = ['Horror', 'Action', 'Comedy', 'Romance', 'Sci-fi', 'Indies'];
+    var correctMovieGenres = ["Horror", "Action", "Comedy"];
+    console.log("my movie genre: " + correctMovieGenres)
 
-    if (myMovieGenre === movies){
-        answer ="Yes horror is my favorite, nice guess.";
-        counter++;
-    } else {
-        answer = "No, I like that genre but Horror is my favorite.";
-    }
-    document.getElementById("movies").innerHTML = answer;
+    for (var i = 0; i < correctMovieGenres.length; i++) {
+        console.log(movieGuess === correctMovieGenres[i])
+        if (movieGuess === correctMovieGenres[i]) {
+            console.log(correctMovieGenres[i])
+            answer = "Yes I do like that genre";
+            counter++;
+            break;
+        } else {
+            answer = "No, I don't like that genre";
+        }
+     }
+     document.getElementById("movies").innerHTML = answer;
 
+    alert("You got " + counter + " correct");
+
+} else {
+    alert("Awww, sorry to hear that, come back another time");
+
+}
     // var userGuess = prompt("Do I like dogs or cats better?");
     // console.log("Dogs or cats better: " + userGuess);
     // if ("dogs" === userGuess.toLowerCase() || "dog" === userGuess.toLowerCase()) {
@@ -94,11 +105,3 @@ if (wantToPlay) {
     // } else {
     //     // alert("I'm not sure what you typed in. Try again");
     
-    
-
-    alert("You got " + counter + " correct");
-
-    } else {
-    alert("Awww, sorry to hear that, come back another time");
-
-}
